1e64ef9a957deae3d9fe00ea369a1883520d3008
Raihan <12584890+17radf@users.noreply.github.com>
2021-06-18 13:20:29 +0700

    add ultra simple explanation for variable

--- ifan.cpp
+++ ifan.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int ucup =1 ;
-	int total;
-	total=ucup+ucup+ucup;
-	cout<<total<<endl;
-	cout<<total;
+	// <tipe data> <nama variabel> <assign> <value> <titik koma>
+	int gandi = 1;
+	int abdi = 1;
+	String dian = "ortumu blabla";
+	// cout untuk menampilkan 
+	cout<<dian<<" "<<gandi+abdi;
 }
+
 	
 
