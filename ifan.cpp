17881b46cf6c93f59b43736a9d82039eaf6e2fcd
Raihan <12584890+17radf@users.noreply.github.com>
2021-06-18 13:14:31 +0700

    add variable training

--- ifan.cpp
+++ ifan.cpp
@@ -6,6 +6,14 @@ int main(){
 	total=ucup+ucup+ucup;
 	cout<<total<<endl;
 	cout<<total;
+
+	// belajar variable 2
+	int two = 2;
+	String kata = "gandi bau ";
+	int whomst = 3;
+	cout<<two;
+	cout<<whomst;
+	cout<<kata<<two<whomst<<" kali lipat\n";
 }
 	
 
