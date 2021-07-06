601dd77ef77c7ab663e03b4b4fd76c62163ed0a3
Raihan <12584890+17radf@users.noreply.github.com>
2021-07-06 22:04:05 +0700

    belajar decision

--- Fano.cpp
+++ Fano.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-using namespace std;
+using namespace  std;
 int main(){
-    int ucok;
-    int ucil;
-    cout<<"mau di isi berapa anka pertama ? ";
-    cin>>ucok;
-    cout<<"mau di isi berapa angka kedua ? ";
-    cin>>ucil;
-    cout<<"is jawaban dari angka ke satu dan kedua adalah "<<ucok*ucil;
+    string guru;
+    cout<<"siapa nama guru tersebut? ";
+    cin>>guru;
+    if(guru =="gandi"){
+        cout<<"nama guru tersebut bernama gandi";
+    }else{
+        cout<<"salah nama guru tersebut adalah gandi";
+    }
 }
-    
