6c3c1d84b4b32303d5e5b50aa795f5ffa9d908f8
Irfan <85829108+irfanar15@users.noreply.github.com>
2021-08-03 20:02:03 +0700

    belajar menu

--- Fano.cpp
+++ Fano.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int A;
-    int S;
-    int D;
-    int F;
-    int total;
-    cout<<"A = ";
-    cin>>A;
-    cout<<"S = ";
-    cin>>S;
-    cout<<"D = ";
-    cin>>D;
-    cout<<"F = ";
-    cin>>F;
-    total=(A+S)*(D+F);
-    if(total >= 50){
-        cout<<"gandi eek\n";
-    }else{
-        cout<<"gandi bau\n";
+    int Pilihan;
+    int perhitungan;
+    int perhitungan2;
+    cout<<"===Perhitungan===\n";
+    cout<<"1 = Perkalian\n";
+    cout<<"2 = Pembagian\n";
+    cout<<"3 = Pertambahan\n";
+    cout<<"4 = Perkurangan\n";
+    cout<<"5 = Keluar\n";
+    cout<<"Pilih yang mana? (gunakan angka sebagai pemilihan)\n";
+    cin>>Pilihan;
+    if(Pilihan == 1){
+        cout<<"Anda memilih Perkalian\n";
+        cout<<"Tulis angka yang ingin kamu hitung = \n";
+        cin>>perhitungan;
+        cout<<"angka ke 2 = \n";
+        cin>>perhitungan2;
+        cout<<perhitungan*perhitungan2;
+    }else if(Pilihan == 2){
+        cout<<"Anda memilih Pembagian\n";
+        cout<<"Tulis angka yang ingin kamu hitung = \n";
+        cin>>perhitungan;
+        cout<<"angka ke 2 = \n";
+        cin>>perhitungan2;
+        cout<<perhitungan/perhitungan2;
+    }else if(Pilihan == 3){
+        cout<<"Anda memilih Pertambahan\n";
+        cout<<"Tulis angka yang ingin kamu hitung = \n";
+        cin>>perhitungan;
+        cout<<"angka ke 2 = \n";
+        cin>>perhitungan2;
+        cout<<perhitungan+perhitungan2;
+    }else if(Pilihan == 4){
+        cout<<"Anda memilih Pengurangan\n";
+        cout<<"Tulis angka yang ingin kamu hitung = \n";
+        cin>>perhitungan;
+        cout<<"angka ke 2 = \n";
+        cin>>perhitungan2;
+        cout<<perhitungan-perhitungan2;
+    }else{  
+        cout<<"anda memilih keluar \n";
     }
-    cout<<"hasil sama dengan = "<<total<<"\n";
 }
