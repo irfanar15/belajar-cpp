b40f5e2133e77470f8e79bfb12a5ce42a0ff88f4
Irfan <85829108+irfanar15@users.noreply.github.com>
2021-08-05 21:41:55 +0700

    belajar goto

--- Fano.cpp
+++ Fano.cpp
@@ -4,65 +4,19 @@ int main(){
     int Pilihan;
     a:
     cout<<"===Menu===\n";
-    cout<<"1 = Ucup\n";
-    cout<<"2 = Ucil\n";
-    cout<<"3 = Tom\n";
-    cout<<"4 = Exit\n";
+    cout<<"1 = Tom\n";
+    cout<<"2 = Exit\n";
     cin>>Pilihan;
     if(Pilihan == 1){
-        cout<<"===Ucup===\n";
-        cout<<"1 = Ekor\n";
-        cout<<"2 = Mata\n";
-        cout<<"3 = Bulu\n";
-        cout<<"4 = Berat\n";
-        cin>>Pilihan;
-        if(Pilihan == 1){
-            cout<<"Kecil bentuk pacman\n";
-        }else if(Pilihan == 2){
-            cout<<"Biru\n";
-        }else if(Pilihan == 3){
-            cout<<"Putih Abu-Abu\n";
-        }else {
-            cout<<"4,5 kg\n";
-        }
-    }else if(Pilihan == 2){
-        cout<<"===Ucil===\n";
-        cout<<"1 = Ekor\n";
-        cout<<"2 = Mata\n";
-        cout<<"3 = Bulu\n";
-        cout<<"4 = Berat\n";
-        cin>>Pilihan;
-        if(Pilihan == 1){
-            cout<<"Panjang Tinggi\n";
-        }else if(Pilihan == 2){
-            cout<<"Biru\n";
-        }else if(Pilihan == 3){
-            cout<<"Putih Abu-Abu\n";
-        }else{
-            cout<<"4 kg\n";
-        }
-    }else if(Pilihan == 3){
-        cout<<"===Tom===\n";
-        cout<<"1 = Ekor\n";
-        cout<<"2 = Mata\n";
-        cout<<"3 = Bulu\n";
-        cout<<"4 = Berat\n";
-        cin>>Pilihan;
-        if(Pilihan == 1){
-            cout<<"Panjang Berbulu\n";
-        }else if(Pilihan == 2){
-            cout<<"Emas\n";
-        }else if(Pilihan == 3){
-            cout<<"Kuning agak oren\n";
-        }else{
-            cout<<"3,5 kg\n";
-        }
-    }else{
-        cout<<"Keluar";
+        cout<<"Bau\n";
     }
-    cout<<"\nmau balik atau tidak?";
-    cin>>Pilihan;
-    if(Pilihan == 1){
-        goto a;
+    else{
+        cout<<"Exit\n";
     }
+        cout<<"Exit = 1:Yes/2:No\n";
+        cin>>Pilihan;
+        if(Pilihan ==1){
+            goto a;
+        }
+
 }
