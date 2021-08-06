f3ed90a6d789d0b0f1869afd18d2b9f5b7c3f8c5
Irfan <85829108+irfanar15@users.noreply.github.com>
2021-08-06 20:55:16 +0700

    belajar goto master

--- Fano.cpp
+++ Fano.cpp
@@ -1,22 +1,68 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int Pilihan;
-    a:
+    int Pilih;
+    A:
     cout<<"===Menu===\n";
-    cout<<"1 = Tom\n";
-    cout<<"2 = Exit\n";
-    cin>>Pilihan;
-    if(Pilihan == 1){
-        cout<<"Bau\n";
+    cout<<"1 = Tong\n";
+    cout<<"2 = Ucup\n";
+    cout<<"3 = Ucil\n";
+    cout<<"4 = Exit\n";
+    cin>>Pilih;
+    if(Pilih ==1){
+        cout<<"===Tong===\n";
+        cout<<"1 = Mata\n";
+        cout<<"2 = Bulu\n";
+        cout<<"3 = Sikap\n";
+        cin>>Pilih;
+        if(Pilih == 1){
+            cout<<"Emas\n";
+        }
+        else if(Pilih == 2){
+            cout<<"Kuning\n";
+        }
+        else if(Pilih == 3){
+            cout<<"Malas\n";
+        }
+    }
+    else if(Pilih == 2){
+        cout<<"===Ucup===\n";
+        cout<<"1 = Mata\n";
+        cout<<"2 = Bulu\n";
+        cout<<"3 = Sikap\n";
+        cin>>Pilih;
+        if(Pilih == 1){
+            cout<<"Biru\n";
+        }
+        else if(Pilih == 2){
+            cout<<"Abu-Abu Putih\n";
+        }
+        else if(Pilih == 3){
+            cout<<"Bacot\n";
+        }
+    }
+    else if(Pilih == 3){
+        cout<<"===Ucil===\n";
+        cout<<"1 = Mata\n";
+        cout<<"2 = Bulu\n";
+        cout<<"3 = Sikap\n";
+        cin>>Pilih;
+        if(Pilih == 1){
+            cout<<"Biru\n";
+        }
+        else if(Pilih == 2){
+            cout<<"Abu-Abu Putih\n";
+        }
+        else if(Pilih == 3){
+            cout<<"Bacot,Adventure Time\n";
+        }
     }
     else{
         cout<<"Exit\n";
     }
-        cout<<"Exit = 1:Yes/2:No\n";
-        cin>>Pilihan;
-        if(Pilihan ==1){
-            goto a;
-        }
-
+    cout<<"jika ingin balik gunakan = 1 / 2 untuk end\n";
+    cin>>Pilih;
+    if(Pilih == 1){
+        goto A;
+    }
 }
