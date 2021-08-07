162a528196efe8b5404999ebe7361a62210fa5f2
Irfan <85829108+irfanar15@users.noreply.github.com>
2021-08-07 20:04:13 +0700

    belajar Switch Case + goto

--- Fano.cpp
+++ Fano.cpp
@@ -1,33 +1,59 @@
 #include <iostream>
 using namespace std;
 int main(){
-    // int day = 2;
-    // switch(day){
-    //     case 1:
-    //     cout<<"ohhhh  1 ya";
-    //     break;
-
-    //     case 2:
-    //     cout<<"ohhh 2 ya";
-    //     break;
-
-    //     default:
-    //     cout<<"what the fuck else";
-    //     break;
-    // }
-
-    int ucup = 7934643;
-        switch(ucup){
-            case 1:
-            cout<<"ucil bau";
+    B:
+    int Pilih;
+    int D,F;
+    cout<<"===Calculator===\n";
+    cout<<"1 = pertambahan\n";
+    cout<<"2 = Pengurangan\n";
+    cout<<"3 = Perkalian\n";
+    cout<<"4 = Pembagian\n";
+    cout<<"5 = Exit\n";
+    cin>>Pilih;
+    int A = 1;
+    switch (Pilih){
+        case 1:
+            cout<<"===Pertambahan===\n";
+            cin>>D;
+            cout<<"angka ke 2\n";
+            cin>>F;
+            cout<<D+F;
             break;
-
-            case 5:
-            cout<<"babi";
+        case 2:
+            cout<<"===Pengurangan===\n";
+            cin>>D;
+            cout<<"angka ke 2\n";
+            cin>>F;
+            cout<<D-F;
+            break;
+        case 3:
+            cout<<"===Perkalian===\n";
+            cin>>D;
+            cout<<"angka ke 2\n";
+            cin>>F;
+            cout<<D*F;
+            break;
+        case 4:
+            cout<<"===Pembagian===\n";
+            cin>>D;
+            cout<<"angka ke 2\n";
+            cin>>F;
+            cout<<D/F;
+            break;
+        default:
+            cout<<"Exit\n";
+        break;
+    }
+        cout<<"\nBack = 1:Y / 2:N = ";
+        cin>>Pilih;
+        switch(Pilih){
+            case 1:
+            goto B;
             break;
 
             default:
-            cout<<"hanu sapi";
             break;
         }
-} 
+
+}
