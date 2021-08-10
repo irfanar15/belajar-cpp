e57ab5f2a2ec1e9d948bc2ca5f840dd84f129f5f
Irfan <85829108+irfanar15@users.noreply.github.com>
2021-08-10 20:06:44 +0700

    belajar menu / case / laptop / penempatan

--- Fano.cpp
+++ Fano.cpp
@@ -1,82 +1,71 @@
 #include <iostream>
 using namespace std;
 int main(){
-    Z:
-    int Pilih,Pilihan;
-    int A,B;
-        cout<<"===Menu===\n";
-        cout<<"1 = Calculator\n";
-        cout<<"2 = laptop\n";
-        cout<<"Any Keys = Exit\n";
-        cin>>Pilih;
+    A:
+    int Pilih;
+    int AngkaA;
+    int AngkaB;
+    cout<<"===Menu===\n";
+    cout<<"1 = laptop\n";
+    cout<<"2 = Calculator\n";
+    cout<<"Any Keys = Exit\n";
+    cin>>Pilih;
     switch(Pilih){
         case 1:
-            cout<<"===Calculator===\n";
-            cout<<"1 = Pertambahan\n";
-            cout<<"2 = Pengurangan\n";
-            cout<<"3 = Perkalian\n";
-            cout<<"4 = Pembagian\n";
-            cin>>Pilihan;
-        switch(Pilihan){
-            case 1:
-                    cout<<"===Pertambahan===\n = ";
-                    cin>>A;
-                    cout<<"\nAngka 2 = ";
-                    cin>>B;
-                    cout<<A+B;
-            break;
-
-            case 2:
-                    cout<<"===Pengurangan===\n = ";
-                    cin>>A;
-                    cout<<"\nAngka 2 = ";
-                    cin>>B;
-                    cout<<A-B;
-            break;
-
-            case 3:
-                    cout<<"===Perkalian===\n = ";
-                    cin>>A;
-                    cout<<"\nAngka 2 = ";
-                    cin>>B;
-                    cout<<A*B;
-            break;
-
-            case 4:
-                cout<<"===Pembagian===\n = ";
-                cin>>A;
-                cout<<"\nAngka 2 = ";
-                cin>>B;
-                cout<<A/B;
-            break;
-        }
-        break;
-        case 2:
-                cout<<"===Laptop===\n";
-                cout<<"1 = Laptop Raihan\n";
-                cout<<"2 = Laptop Irfan\n";
-                cin>>Pilih;
+            cout<<"===laptop===\n";
+            cout<<"1 = Laptop Aru\n";
+            cout<<"2 = laptop Raihan\n";
+            cout<<"3 = Laptop Irfan\n";
+            cout<<"4 = Laptop Ayah\n";
+            cin>>Pilih;
             switch(Pilih){
                 case 1:
-                    cout<<"===LaptopRaihan===\n";
+                    cout<<"===Laptop Aru===\n";
                     cout<<"1 = Ram\n";
                     cout<<"2 = Rom\n";
+                    cout<<"3 = cpu\n";
                     cin>>Pilih;
-                switch(Pilih){
-                    case 1:
-                        cout<<"4gb\n";
-                    break;
+                    switch(Pilih){
+                        case 1:
+                            cout<<"4gb\n";
+                        break;
 
-                    case 2:
-                        cout<<"256gb\n";
-                    break;
-                }
+                        case 2:
+                            cout<<"??gb\n";
+                        break;
+
+                        case 3:
+                            cout<<"Intel\n";
+                        break;
+                    }
                 break;
 
                 case 2:
-                    cout<<"===LaptopIrfan===\n";
+                    cout<<"===Laptop Raihan===\n";
                     cout<<"1 = Ram\n";
                     cout<<"2 = Rom\n";
+                    cout<<"3 = cpu\n";
+                    cin>>Pilih;
+                    switch(Pilih){
+                        case 1:
+                            cout<<"4gb\n";
+                        break;
+
+                        case 2:
+                            cout<<"256gb\n";
+                        break;
+
+                        case 3:
+                            cout<<"Intel\n";
+                        break;
+                    }
+                break;
+
+                case 3:
+                    cout<<"===Laptop Irfan===\n";
+                    cout<<"1 = Ram\n";
+                    cout<<"2 = Rom\n";
+                    cout<<"3 = cpu\n";
                     cin>>Pilih;
                     switch(Pilih){
                         case 1:
@@ -86,17 +75,90 @@ int main(){
                         case 2:
                             cout<<"512gb\n";
                         break;
+
+                        case 3:
+                            cout<<"Intel\n";
+                        break;
                     }
                 break;
-            }
-    break;
 
+                case 4:
+                    cout<<"===Laptop Ayah===\n";
+                    cout<<"1 = Ram\n";
+                    cout<<"2 = Rom\n";
+                    cout<<"3 = cpu\n";
+                    cin>>Pilih;
+                    switch(Pilih){
+                        case 1:
+                            cout<<"??gb\n";
+                        break;
+
+                        case 2:
+                            cout<<"256gb\n";
+                        break;
+
+                        case 3:
+                            cout<<"Intel Pentium\n";
+                        break;
+                    }
+                break;
+
+
+            }
+        break;
+
+        case 2:
+            cout<<"===Calculator===\n";
+            cout<<"1 = Pertambahan\n";
+            cout<<"2 = Pengurangan\n";
+            cout<<"3 = Perkalian\n";
+            cout<<"4 = Pembagian\n";
+            cin>>Pilih;
+            switch(Pilih){
+                case 1:
+                    cout<<"===Pertambahan===\n";
+                    cout<<"Angka 1 = \n";
+                    cin>>AngkaA;
+                    cout<<"Angka 2 = \n";
+                    cin>>AngkaB;
+                    cout<<AngkaA+AngkaB;
+                break;
+
+                case 2:
+                    cout<<"===Pengurangan===\n";
+                    cout<<"Angka 1 = \n";
+                    cin>>AngkaA;
+                    cout<<"Angka 2 = \n";
+                    cin>>AngkaB;
+                    cout<<AngkaA-AngkaB;
+                break;
+
+                case 3:
+                    cout<<"===Perkalian===\n";
+                    cout<<"Angka 1 = \n";
+                    cin>>AngkaA;
+                    cout<<"Angka 2 = \n";
+                    cin>>AngkaB;
+                    cout<<AngkaA*AngkaB;
+                break;
+
+                case 4:
+                    cout<<"===Pembagian===\n";
+                    cout<<"Angka 1 = \n";
+                    cin>>AngkaA;
+                    cout<<"Angka 2 = \n";
+                    cin>>AngkaB;
+                    cout<<AngkaA/AngkaB;
+                break;
+ 
+            }
+        break;
     }
-        cout<<"\nBack = 1 / Any Keys = End Browse \n";
-        cin>>Pilih;
-        switch(Pilih){
-            case 1:
-            goto Z;
-            break;
-        }
+    cout<<"\nBack = 1 / Any Keys = Done/Exit\n";
+    cin>>Pilih;
+    switch(Pilih){
+        case 1:
+            goto A;
+        break;
+    }
 }
