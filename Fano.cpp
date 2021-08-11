5406af3fe4c9cfed67359a291791ff551ae575a8
Irfan <85829108+irfanar15@users.noreply.github.com>
2021-08-11 22:00:41 +0700

    belajar Case / goto / penempatan

--- Fano.cpp
+++ Fano.cpp
@@ -3,162 +3,64 @@ using namespace std;
 int main(){
     A:
     int Pilih;
-    int AngkaA;
-    int AngkaB;
-    cout<<"===Menu===\n";
-    cout<<"1 = laptop\n";
-    cout<<"2 = Calculator\n";
+    cout<<"===MenuRumah===\n";
+    cout<<"1 = Lemari\n";
+    cout<<"2 = Meja Laptop\n";
     cout<<"Any Keys = Exit\n";
     cin>>Pilih;
     switch(Pilih){
         case 1:
-            cout<<"===laptop===\n";
-            cout<<"1 = Laptop Aru\n";
-            cout<<"2 = laptop Raihan\n";
-            cout<<"3 = Laptop Irfan\n";
-            cout<<"4 = Laptop Ayah\n";
+            cout<<"===Lemari===\n";
+            cout<<"1 = Lemari 1\n";
+            cout<<"2 = Lemari 2\n";
+            cout<<"3 = Lemari 3\n";
+            cout<<"4 = Lemari 4\n";
+            cout<<"5 = Lemari 5\n";
             cin>>Pilih;
             switch(Pilih){
                 case 1:
-                    cout<<"===Laptop Aru===\n";
-                    cout<<"1 = Ram\n";
-                    cout<<"2 = Rom\n";
-                    cout<<"3 = cpu\n";
-                    cin>>Pilih;
-                    switch(Pilih){
-                        case 1:
-                            cout<<"4gb\n";
-                        break;
-
-                        case 2:
-                            cout<<"??gb\n";
-                        break;
-
-                        case 3:
-                            cout<<"Intel\n";
-                        break;
-                    }
+                    cout<<"Sempak,Topi\n";
                 break;
 
                 case 2:
-                    cout<<"===Laptop Raihan===\n";
-                    cout<<"1 = Ram\n";
-                    cout<<"2 = Rom\n";
-                    cout<<"3 = cpu\n";
-                    cin>>Pilih;
-                    switch(Pilih){
-                        case 1:
-                            cout<<"4gb\n";
-                        break;
-
-                        case 2:
-                            cout<<"256gb\n";
-                        break;
-
-                        case 3:
-                            cout<<"Intel\n";
-                        break;
-                    }
+                    cout<<"Sempak,Topi,BH\n";
                 break;
 
                 case 3:
-                    cout<<"===Laptop Irfan===\n";
-                    cout<<"1 = Ram\n";
-                    cout<<"2 = Rom\n";
-                    cout<<"3 = cpu\n";
-                    cin>>Pilih;
-                    switch(Pilih){
-                        case 1:
-                            cout<<"2gb\n";
-                        break;
-
-                        case 2:
-                            cout<<"512gb\n";
-                        break;
-
-                        case 3:
-                            cout<<"Intel\n";
-                        break;
-                    }
+                    cout<<"Masker,BH\n";
                 break;
 
                 case 4:
-                    cout<<"===Laptop Ayah===\n";
-                    cout<<"1 = Ram\n";
-                    cout<<"2 = Rom\n";
-                    cout<<"3 = cpu\n";
-                    cin>>Pilih;
-                    switch(Pilih){
-                        case 1:
-                            cout<<"??gb\n";
-                        break;
-
-                        case 2:
-                            cout<<"256gb\n";
-                        break;
-
-                        case 3:
-                            cout<<"Intel Pentium\n";
-                        break;
-                    }
+                    cout<<"Sempak,Handuk Kecil\n";
                 break;
 
-
+                case 5:
+                    cout<<"Sarung Tangan\n";
+                break;
             }
         break;
 
         case 2:
-            cout<<"===Calculator===\n";
-            cout<<"1 = Pertambahan\n";
-            cout<<"2 = Pengurangan\n";
-            cout<<"3 = Perkalian\n";
-            cout<<"4 = Pembagian\n";
+            cout<<"===MejaLaptop===\n";
+            cout<<"1 = Lemari Meja\n";
+            cout<<"2 = Meja Bawah\n";
             cin>>Pilih;
             switch(Pilih){
                 case 1:
-                    cout<<"===Pertambahan===\n";
-                    cout<<"Angka 1 = \n";
-                    cin>>AngkaA;
-                    cout<<"Angka 2 = \n";
-                    cin>>AngkaB;
-                    cout<<AngkaA+AngkaB;
+                    cout<<"Buku,Fresh Case,Foto\n";
                 break;
 
                 case 2:
-                    cout<<"===Pengurangan===\n";
-                    cout<<"Angka 1 = \n";
-                    cin>>AngkaA;
-                    cout<<"Angka 2 = \n";
-                    cin>>AngkaB;
-                    cout<<AngkaA-AngkaB;
+                    cout<<"Kosong\n";
                 break;
-
-                case 3:
-                    cout<<"===Perkalian===\n";
-                    cout<<"Angka 1 = \n";
-                    cin>>AngkaA;
-                    cout<<"Angka 2 = \n";
-                    cin>>AngkaB;
-                    cout<<AngkaA*AngkaB;
-                break;
-
-                case 4:
-                    cout<<"===Pembagian===\n";
-                    cout<<"Angka 1 = \n";
-                    cin>>AngkaA;
-                    cout<<"Angka 2 = \n";
-                    cin>>AngkaB;
-                    cout<<AngkaA/AngkaB;
-                break;
- 
             }
         break;
     }
-    cout<<"\nBack = 1 / Any Keys = Done/Exit\n";
+    cout<<"\nBack = 1 / Any Keys = Exit\n";
     cin>>Pilih;
     switch(Pilih){
         case 1:
             goto A;
         break;
-    }
+    }   
 }
