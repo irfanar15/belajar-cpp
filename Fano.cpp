a930b4e294ebced6f1a50da494c1fbe170ef9e9a
Irfan <85829108+irfanar15@users.noreply.github.com>
2021-08-16 20:14:05 +0700

    Menu Random

--- Fano.cpp
+++ Fano.cpp
@@ -3,89 +3,122 @@ using namespace std;
 int main(){
     A:
     int Pilih;
-    int B;
-    int C;
-    cout<<"===Menu===\n";
-    cout<<"1 = Ucil\n";
-    cout<<"2 = Ucup\n";
-    cout<<"3 = Tom\n";
-    cout<<"4 = perulangan(For)\n";
+    cout<<"===MenuRumah===\n";
+    cout<<"1 = lemari\n";
+    cout<<"2 = TV\n";
+    cout<<"3 = Meja Kerja\n";
+    cout<<"4 = Meja TV\n";
+    cout<<"Any Keys = Exit\n";
     cin>>Pilih;
     switch(Pilih){
         case 1:
-            cout<<"===Ucil===\n";
-            cout<<"1 = Mata\n";
-            cout<<"2 = Berat\n";
-            cout<<"3 = Sikap\n";
+            cout<<"===lemari(Pakaian)===\n";
+            cout<<"1 = Lemari 1\n";
+            cout<<"2 = Lemari 2\n";
+            cout<<"3 = Lemari 3\n";
+            cout<<"4 = Lemari 4\n";
+            cout<<"5 = Lemari 5\n";
             cin>>Pilih;
             switch(Pilih){
                 case 1:
-                    cout<<"Biru\n";
+                    cout<<"Sempak\n";
                 break;
 
                 case 2:
-                    cout<<"5kg\n";
+                    cout<<"Celana\n";
+                break;
+                
+                case 3:
+                    cout<<"BH\n";
                 break;
 
-                case 3:
-                    cout<<"bacot,gelut,berpetualang\n";
+                case 4:
+                    cout<<"kaos kaki\n";
+                break;
+
+                case 5:
+                    cout<<"Kaos\n";
                 break;
             }
         break;
 
         case 2:
-            cout<<"===Ucup===\n";
-            cout<<"1 = Mata\n";
-            cout<<"2 = Berat\n";
-            cout<<"3 = Sikap\n";
+            cout<<"===Tv===\n";
+            cout<<"1 = Spec\n";
+            cout<<"2 = Lebar\n";
+            cout<<"3 = Tinggi\n";
             cin>>Pilih;
             switch(Pilih){
                 case 1:
-                    cout<<"Biru\n";
+                    cout<<"2k/HDMI\n";
                 break;
-
+                
                 case 2:
-                    cout<<"4.5kg\n";
+                    cout<<"1m\n";
                 break;
 
                 case 3:
-                    cout<<"bacot ngeselin\n";
+                    cout<<"75mm\n";
                 break;
             }
         break;
 
         case 3:
-            cout<<"===Tom===\n";
-            cout<<"1 = Mata\n";
-            cout<<"2 = Berat\n";
-            cout<<"3 = Sikap\n";
+            cout<<"===MejaKerja===\n";
+            cout<<"1 = Permukaan\n";
+            cout<<"2 = Loker\n";
             cin>>Pilih;
             switch(Pilih){
                 case 1:
-                    cout<<"Emas\n";
+                    cout<<"Laptop/Radio/kertas/Printer\n";
                 break;
 
                 case 2:
-                    cout<<"3.5\n";
+                    cout<<"Casing/Kertas\n";
                 break;
 
-                case 3:
-                    cout<<"diam,sering hadap ke tembok\n";
-                break;
             }
         break;
 
         case 4:
-                cout<<"dari?\n";
-                cin>>B;
-                cout<<"Sampai?\n";
-                cin>>C;
-                for(int ok = B; ok < C; ok++){
-                    cout<<ok<<"hehe\n";
-                }
+            cout<<"===MejaTV===\n";
+            cout<<"1 = Permukaan(Meja)\n";
+            cout<<"2 = Loker\n";
+            cin>>Pilih;
+            switch(Pilih){
+                case 1:
+                    cout<<"Sarung/Kucing/Lampu\n";
+                break;
+
+                case 2:
+                    cout<<"===Loker(MejaTV)===\n";
+                    cout<<"1 = Loker 1\n";
+                    cout<<"2 = Loker 2\n";
+                    cout<<"3 = Loker 3\n";
+                    cout<<"4 = Loker 4\n";
+                    cin>>Pilih;
+                    switch(Pilih){
+                        case 1:
+                            cout<<"Charger/Camera\n";
+                        break;
+
+                        case 2:
+                            cout<<"Tripod/Kardus\n";
+                        break;
+
+                        case 3:
+                            cout<<"Tas/Tas punggung\n";
+                        break;
+
+                        case 4:
+                            cout<<"Tas Laptop\n";
+                        break;
+                    }
+                break;
+            }
         break;
     }
-    cout<<"\nBack = 1 / Any Keys = Exit\n";
+    cout<<"\nBack = 1\n";
     cin>>Pilih;
     switch(Pilih){
         case 1:
